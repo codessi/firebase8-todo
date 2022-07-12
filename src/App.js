9fa27300cdb77bb650c301dfad603d41de3899dd
codessi <johansuh@gmail.com>
2022-07-11 20:06:58 -0700

    before firebase install

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Todos from "./Todos";
 
-function App() {
+const signInWithGoogle = () => { };
+
+const SignIn = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <main>
+      <button onClick={ signInWithGoogle }>Sign in with google</button>
+    </main>
+  )
+}
+
+const App = () => {
+
+  const user = "test"
+  return (
+    <div>
+     {user? <Todos /> : <SignIn /> }
+      
     </div>
   );
 }
